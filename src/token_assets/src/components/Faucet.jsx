10e5910a987c639c7a96e213d162c47c28217d05
IvyZayn <kakapon713@outlook.com>
2023-09-01 16:26:19 +0800

    Added the authentication logic.

--- src/token_assets/src/components/Faucet.jsx
+++ src/token_assets/src/components/Faucet.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
-import {token} from "../../../declarations/token";
+import {token, canisterId, createActor} from "../../../declarations/token";
+import {AuthClient} from "@dfinity/auth-client";
 
 function Faucet() {
   const [isDisabled, setDisabled] = useState(false); // control the states of the button
@@ -7,7 +8,17 @@ function Faucet() {
 
   async function handleClick(event) {
     setDisabled(true);
-    const result = await token.payOut();
+
+    // create an actor using user identity so that tokens can be assigned to a user
+    const authClient = await AuthClient.create();
+    const identity = await authClient.getIdentity();
+    const authenticatedCanister = createActor(canisterId, {
+      agentOptions: {
+        identity,
+      },
+    });
+
+    const result = await authenticatedCanister.payOut();
     setText(result);
   }
 
--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,8 +1,25 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from "./components/App";
+import {AuthClient} from "@dfinity/auth-client";
 
 const init = async () => { 
+
+  const authClient = await AuthClient.create(); // create a authClient object
+
+  if (await authClient.isAuthenticated()) {
+    handleAuthenticated(authClient); // if logined in, render the webpage
+  } else {
+      await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize", // a provider for the login frontend 
+      onSuccess: () => { // what happen if the login is successful 
+        handleAuthenticated(authClient);
+      }
+    });
+  }
+}
+
+async function handleAuthenticated(authClient) { 
   ReactDOM.render(<App />, document.getElementById("root"));
 }
 
